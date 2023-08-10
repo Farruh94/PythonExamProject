c8e0733a9c9b5e9112d9afbaa6c128a8dcbd00fa
Farruh Kalendarov <farruh.kalendarov@gmail.com>
2023-08-10 13:29:02 +0300

    fixed master

--- tests/__init__.py
+++
@@ -1,2 +0,0 @@
-from tests import tag_check
-from tag_check import directory_lookup, check_html_content
--- tests/main.py
+++
@@ -1,3 +0,0 @@
-from tests import tag_check
-
-tag_check.check_html_content(tag_check.directory_lookup())
